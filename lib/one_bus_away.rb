809095e4c6a1344ff27240c3182b8ca1d11e4f30
Joe Engel <engelj@a45e60c8d265.ant.amazon.com>
2015-10-21 14:48:18 -0700

    Fixing README, setting up platform for being geo aware, adding gem to gemspec

--- lib/one_bus_away.rb
+++ lib/one_bus_away.rb
@@ -3,6 +3,7 @@ require_relative 'one_bus_away/utilities'
 require_relative 'one_bus_away/client'
 require 'rest-client'
 require 'contracts'
+require 'geocoder'
 
 # Main class
 # defines the way to interact with everything
@@ -13,5 +14,12 @@ class OneBusAway
 
   # Contract String => String
   def initialize
+  end
+
+  def self.get_location(loc)
+    geo = Geocoder.search("#{loc} seattle")
+    lat = geo[0].latitude
+    lon = geo[0].longitude
+    return lat, lon
   end
 end
